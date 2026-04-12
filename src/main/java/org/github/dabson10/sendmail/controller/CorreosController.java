1a0c5189cfe04423ecbc8c07715a3d1396c68bbc
Dabson10 <dabson753@gmail.com>
2026-04-12 17:26:42 -0600

    Se cambio la entrada de la api a una sola web

--- src/main/java/org/github/dabson10/sendmail/controller/CorreosController.java
+++ src/main/java/org/github/dabson10/sendmail/controller/CorreosController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://cv-juan-david.netlify.app/")
 @RequestMapping("/mail")
 public class CorreosController {
 
